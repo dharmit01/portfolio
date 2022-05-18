8f2bb70bbad96d6e4f2de676edc8c4718f92a9e9
dharmit01 <shahdharmit01@hotmail.com>
2022-05-19 01:16:59 +0530

    Portfolio Website

---
+++ index.js
@@ -0,0 +1,22 @@
+if ($(".text-slider").length == 1) {
+    var typed_strings = $(".text-slider-items").text();
+    var typed = new Typed(".text-slider", {
+        strings: typed_strings.split(", "),
+        typeSpeed: 50,
+        loop: true,
+        backDelay: 900,
+        backSpeed: 30,
+    });
+}
+
+$(document).ready(function() {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
